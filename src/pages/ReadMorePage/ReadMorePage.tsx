bb5ca3a03b7bdd24b17c3a56a15bc57a49813992
William Chadwick <william_chadwick@external.mckinsey.com>
2022-06-25 14:48:39 +0100

    feat: add blurb page

--- src/pages/ReadMorePage/ReadMorePage.tsx
+++ src/pages/ReadMorePage/ReadMorePage.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Button, Heading, Text, useBreakpointValue } from '@chakra-ui/react'
+import { Button, Heading, useBreakpointValue } from '@chakra-ui/react'
 import ContentWrapper from 'common/components/ContentWrapper'
+import ExternalLink from 'common/components/ExternalLink'
+import styles from './ReadMorePage.less'
 
 const ReadMorePage: FC = () => {
   const navigate = useNavigate()
@@ -12,51 +14,72 @@ const ReadMorePage: FC = () => {
   })
 
   return (
-    <ContentWrapper blurred>
-      <Heading>Modal Title</Heading>
-      <Text>
-        The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum
-        dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-        id est laborum." Section 1.10.32 of "de Finibus Bonorum et Malorum",
-        written by Cicero in 45 BC "Sed ut perspiciatis unde omnis iste natus
-        error sit voluptatem accusantium doloremque laudantium, totam rem
-        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-        voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-        dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-        est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-        sed quia non numquam eius modi tempora incidunt ut labore et dolore
-        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-        nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-        aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-        qui in ea voluptate velit esse quam nihil molestiae consequatur, vel
-        illum qui dolorem eum fugiat quo voluptas nulla pariatur?" 1914
-        translation by H. Rackham "But I must explain to you how all this
-        mistaken idea of denouncing pleasure and praising pain was born and I
-        will give you a complete account of the system, and expound the actual
-        teachings of the great explorer of the truth, the master-builder of
-        human happiness. No one rejects, dislikes, or avoids pleasure itself,
-        because it is pleasure, but because those who do not know how to pursue
-        pleasure rationally encounter consequences that are extremely painful.
-        Nor again is there anyone who loves or pursues or desires to obtain pain
-        of itself, because it is pain, but because occasionally circumstances
-        occur in which toil and pain can procure him some great pleasure. To
-        take a trivial example, which of us ever undertakes laborious physical
-        exercise, except to obtain some advantage from it? But who has any right
-        to find fault with a man who chooses to enjoy a pleasure that has no
-        annoying consequences, or one who avoids a pain that produces no
-        resultant pleasure?"
-      </Text>
-      <footer>
+    <ContentWrapper blurred height="100%">
+      <section className={styles.section}>
+        <Heading className={styles.heading}>Living In A Box</Heading>
+        <p>
+          <strong>“Living in a Box”</strong> is a documentary style film
+          produced by Open Arts, an arts and disability organisation based in
+          Belfast. It tells the story of Open Arts through the 2020 – 2021
+          lockdowns and restrictions, mostly from the perspective of our
+          participants. The inspiration for the documentary was to highlight the
+          daily challenges faced by disabled people and help others understand
+          the disproportionate affect Covid has had on them. Engaging in the
+          arts digitally was a lifeline for disabled people and remains so for
+          many. One of our participants noted “now people without a disability
+          know what it’s like to have a disability and the challenges it poses.”
+          Not being able to leave our houses we were all “living in a box.”
+        </p>
+        <br />
+        <p>
+          Our Mission is to inspire and support the creativity and artistic
+          development of disabled people. This website is an opportunity to
+          share the catalogue of remarkable creative work that was produced by
+          participants from our core programme over 15 months of the pandemic.
+          Due to the scale of work produced we wanted to ensure the artistic
+          skill of our participants was recognised.
+        </p>
+        <br />
+        <p>
+          While this film focuses on our core programme for disabled adults, we
+          also run multiple other projects each year to engage more disabled of
+          all ages. Even during 2020-2021, we facilitated online workshops for
+          older people and continued our partnership project, In Tune, which is
+          for children, who are blind/visually impaired, have autism or
+          additional complex needs.
+        </p>
+        <br />
+        <p>
+          There is no similar participatory work happening in N.I., with the
+          ambition and scope; multiple art forms with so many individuals with
+          such varying disabilities and additional support needs: our activities
+          engage people with physical disabilities, learning disabilities,
+          sensory impairments, and mental health issues.
+        </p>
+        <br />
+        <p>
+          Our Vision is a society that actively values and promotes the
+          creativity and artistic contribution of disabled people. If you’d like
+          more information about the work we do, check out our website{' '}
+          <ExternalLink
+            to="https://www.openartsni.org"
+            label="Link to the Open Arts website"
+          >
+            www.openartsni.org
+          </ExternalLink>{' '}
+          or email{' '}
+          <a
+            aria-label="Click to send an email to open arts"
+            href="mailto:info@openartsni.org"
+          >
+            info@openartsni.org
+          </a>
+        </p>
+        <br />
         <Button size={buttonSize} onClick={() => navigate('/')}>
-          Close
+          Back
         </Button>
-      </footer>
+      </section>
     </ContentWrapper>
   )
 }
